8111eb228d4661e700c70c2b803cf7b0cd30ee49
ZaamiAhmed <158285599+ZaamiAhmed@users.noreply.github.com>
2025-05-31 17:53:36 +0300

    updated intro style

--- my-portfolio/src/components/Intro.jsx
+++ my-portfolio/src/components/Intro.jsx
@@ -1,5 +1,5 @@
 import './Intro.css';
-import image from '../images/photo1.jpg';
+import image from '../images/mary.png';
 import { motion } from "framer-motion";
 export default function Intro() {
     return (
@@ -19,17 +19,17 @@ export default function Intro() {
             A passionate Front-End Developer crafting user-friendly web
             experiences.
           </p>
-          <div className="me">
-            <button>About Me!</button>
-            <motion.img
-              src={image}
-              alt="Maryama Ahmed"
-              className="my-image"
-              initial={{ opacity: 0, y: 50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1, delay: 0.3 }}
-            />
-          </div>
+          <button>About Me!</button>
+        </div>
+        <div className="me">
+          <motion.img
+            src={image}
+            alt="Maryama Ahmed"
+            className="my-image"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.3 }}
+          />
         </div>
       </section>
     );
