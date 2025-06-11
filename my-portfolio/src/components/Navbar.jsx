8f9bf70f34dec8fbccd4126bd2425bd07004be75
ZaamiAhmed <158285599+ZaamiAhmed@users.noreply.github.com>
2025-06-11 22:31:44 +0300

    updated navlinks

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,20 +1,31 @@
 import './navbar.css';
 import logo from '../images/maryama.png';
+import {FaBars,FaTimes} from 'react-icons/fa';
+import React,{ useState } from 'react';
 export default function Navbar() {
+
+  const[menuvisible,setMenuvisible]=useState(false);
+
+  const toggleMenu=()=>{
+    setMenuvisible(!menuvisible)
+  }
   return (
     <div className="navbar-container">
       <div className="navbar">
         <img src={logo} alt="" className="img-fluid" />
-        <nav className="navlinks">
+        <div className='nav-menu' onClick={toggleMenu}>
+        {menuvisible? <FaTimes/>:<FaBars/>}
+        </div>
+        <nav className={`navlinks ${menuvisible? 'show':"" }`}>
           <ul>
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
+            <li>Contact</li>
             <li>Contact</li>
           </ul>
         </nav>
       </div>
     </div>
   );
-  
-}
+   }
