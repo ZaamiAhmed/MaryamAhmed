eb045e2541e9d7c017f4b1a279c7255a748089fb
ZaamiAhmed <158285599+ZaamiAhmed@users.noreply.github.com>
2025-06-18 14:52:06 +0300

    made the links active when they are on current use.

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import './navbar.css';
 import logo from '../images/maryama.png';
 import {FaBars,FaTimes} from 'react-icons/fa';
 import React,{ useState } from 'react';
-import { Link } from 'react-router-dom';
+import {  NavLink } from 'react-router-dom';
+
 export default function Navbar() {
 
   const[menuvisible,setMenuvisible]=useState(false);
@@ -14,17 +15,43 @@ export default function Navbar() {
     <div className="navbar-container">
       <div className="navbar">
         <img src={logo} alt="" className="img-fluid" />
-        <div className='nav-menu' onClick={toggleMenu}>
-        {menuvisible? <FaTimes/>:<FaBars/>}
+        <div className="nav-menu" onClick={toggleMenu}>
+          {menuvisible ? <FaTimes /> : <FaBars />}
         </div>
-        <nav className={`navlinks ${menuvisible? 'show':"" }`}>
+        <nav className={`navlinks ${menuvisible ? "show" : ""}`}>
           <ul>
-           <Link to='/' className='links'><li>Home</li></Link> 
-            <Link to='/about' className='links'><li>About</li></Link>
-            <Link to='/projects' className='links'><li>Projects</li></Link>
-           <Link to='/contact' className='links'><li>Contact</li></Link> 
-           
-           
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "links active" : "links"
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "links active" : "links"
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                isActive ? "links active" : "links"
+              }
+            >
+              <li>Projects</li>
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? "links active" : "links"
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
           </ul>
         </nav>
       </div>
