eac7f987d55a80e9b9e31102629c6e92275c6bb7
ZaamiAhmed <158285599+ZaamiAhmed@users.noreply.github.com>
2025-06-19 17:46:17 +0300

    added contact page

--- my-portfolio/src/Pages/Contact.jsx
+++ my-portfolio/src/Pages/Contact.jsx
@@ -1,9 +1,26 @@
 export default function Contact() {
-    return(
-        <div className="centered-page">
-           <h2>Contact Me:</h2> 
-
-        </div>
-    )
+    return (
+      <div className="centered-page">
+        <h2>Contact Me:</h2>
+        <form action="https://formspree.io/f/xnnvzegr" method="post">
+          <input
+            type="text"
+            placeholder="Your Name?"
+            name="name"
+            autoFocus
+            required
+          />
+          <input type="email" placeholder="Your Email?" name="email" required />
+          <input
+            type="tel"
+            placeholder="Your Number?"
+            name="number"
+            required
+          />
+          <textarea name="message" placeholder="your message"></textarea>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
     
 }
